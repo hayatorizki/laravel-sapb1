57c476467db534fc096bb7ab4a6fd65ad3d4dfbf
Rizki Hayato <hayatorizki@gmail.com>
2022-09-01 13:55:03 +0700

    Add template and login module

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','HomeController@index');
+Route::get('/login','AuthController@index');
+Route::post('/login','AuthController@login');
+Route::get('/logout','AuthController@logout');
